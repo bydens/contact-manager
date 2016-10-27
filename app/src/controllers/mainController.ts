a7ea3c6e2de8b58f325a14a6f70af32ecc3d7dff
Denys Bykanov <denys.bykanov@globallogic.com>
2016-10-27 15:20:25 +0300

    first commit

---
+++ app/src/controllers/mainController.ts
@@ -0,0 +1,17 @@
+/// <reference path="../_all.ts" />
+
+module ContactManagerApp {
+  export class MainController {
+    static $inject = [];
+    
+    // constructor(private $scope: any) {
+    //   this.$scope.message = "Hello from our controller";
+    // }
+    
+    constructor() {
+      
+    }
+
+    message: string = "Hello from our controller";
+  }
+}
