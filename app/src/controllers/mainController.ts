338547f5f75751104445d13b934eeec31ff287ef
Denys Bykanov <denys.bykanov@globallogic.com>
2016-10-27 16:15:22 +0300

    sidebar

--- app/src/controllers/mainController.ts
+++ app/src/controllers/mainController.ts
@@ -2,16 +2,24 @@
 
 module ContactManagerApp {
   export class MainController {
-    static $inject = [];
+    static $inject = ['userService'];
     
     // constructor(private $scope: any) {
     //   this.$scope.message = "Hello from our controller";
     // }
     
-    constructor() {
-      
+    constructor(private userService: IUserService) {
+      var self = this;
+
+      this.userService
+        .loadAllUsers()
+        .then((users: User[]) => {
+          self.users = users;
+          console.log(self.users);
+        });
     }
 
+    users: User[] = [];
     message: string = "Hello from our controller";
   }
 }
