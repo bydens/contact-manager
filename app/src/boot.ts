f7ce9e9d9a9dde084fb8ebb70708472719364083
bydens <bydens@yandex.ua>
2016-10-31 19:59:44 +0200

    contactSheet

--- app/src/boot.ts
+++ app/src/boot.ts
@@ -7,8 +7,12 @@ module ContactManagerApp {
   .config(($mdIconProvider: angular.material.IIconProvider, 
            $mdThemingProvider: angular.material.IThemingProvider) => {
     $mdIconProvider
-    .defaultIconSet('./assets/svg/avatars.svg', 128)
-      .icon('menu', './assets/svg/menu.svg', 24);
+      .defaultIconSet('./assets/svg/avatars.svg'          , 128)
+      .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+      .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+      .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+      .icon("phone"      , "./assets/svg/phone.svg"       , 512)
+      .icon('menu',        './assets/svg/menu.svg'        , 24);
 
     $mdThemingProvider.theme('default')
       .primaryPalette('blue')
